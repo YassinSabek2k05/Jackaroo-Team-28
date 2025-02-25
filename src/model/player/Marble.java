5ca6141d18966da4d729e463de67acced949057f
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-02-26 01:28:15 +0200

    Refactor Marble class by removing unnecessary super call and main method

--- src/model/player/Marble.java
+++ src/model/player/Marble.java
@@ -6,17 +6,10 @@ public class Marble {
 	private final Colour colour;
 	
 	public Marble(Colour colour) {
-		super();
 		this.colour = colour;
 	}
 	
 	public Colour getColour(){
 		return colour;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
