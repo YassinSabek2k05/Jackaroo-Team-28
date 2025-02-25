7fad9af629159eb03d62d88f29f72cff2fefe811
raghadkh1305 <raghad.khaled.taher@gmail.com>
2025-02-25 13:33:15 +0200

    vbb

--- src/model/Colour.java
+++ src/model/Colour.java
@@ -3,7 +3,10 @@ package model;
 public enum Colour {
 	RED,GREEN,BLUE,YELLOW;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 }
