c87faff3e6cb12f5d3da6e1c021265305f938497
malak-awadalla <malak.abdulgawad1934@gmail.com>
2025-02-25 13:30:26 +0200

    hhh

--- src/model/Colour.java
+++ src/model/Colour.java
@@ -2,6 +2,12 @@ package model;
 
 public enum Colour {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+	RED,GREEN,BLUE,YELLOW
+
+
+>>>>>>> Stashed changes
 =======
 	RED,GREEN,BLUE,YELLOW
 
