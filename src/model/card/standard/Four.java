204c95a0b2c8b71eae97c132ff49482367faac02
malak-awadalla <malak.abdulgawad1934@gmail.com>
2025-02-26 20:35:45 +0200

    yea

--- src/model/card/standard/Four.java
+++ src/model/card/standard/Four.java
@@ -1,5 +1,13 @@
 package model.card.standard;
 
-public class Four {
+import engine.GameManager;
+import engine.board.BoardManager;
+
+public class Four extends Standard {
+
+	public Four(String name, String description, Suit suit,
+			BoardManager boardManager, GameManager gameManager) {
+		super(name, description,4, suit, boardManager, gameManager);
+	}
 
 }
