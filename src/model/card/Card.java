9c722800c174568fe203e3fb63ef710e0ab5117b
malak-awadalla <malak.abdulgawad1934@gmail.com>
2025-02-26 20:34:41 +0200

    hhh

--- src/model/card/Card.java
+++ src/model/card/Card.java
@@ -1,5 +1,41 @@
 package model.card;
+import engine.GameManager;
+import engine.board.BoardManager;
 
-public class Card {
+abstract public class Card {
+	private final String name;
+	private final String description;
+	protected BoardManager boardManager;
+	protected GameManager gameManager;
+	
+	
+	
+	
+	
+	
+	public Card(String name, String description, BoardManager boardManager,
+			GameManager gameManager) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.boardManager = boardManager;
+		this.gameManager = gameManager;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public String getDescription(){
+		return description ;
+	}
+	
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
 
 }
--- src/model/card/standard/Standard.java
+++ src/model/card/standard/Standard.java
@@ -1,23 +1,36 @@
 package model.card.standard;
 
+import engine.GameManager;
+import engine.board.BoardManager;
 import model.card.Card;
 
 
-public class Standard extends Card {
+
+public class Standard extends Card{
 	private int rank;
 	private Suit suit;
 	
-	
-	
-	public String getRank(){
-		return "Rank = " + rank;
+
+	public Standard(String name, String description,int rank, Suit suit, BoardManager boardManager,
+			GameManager gameManager ) {
+		super(name, description, boardManager, gameManager);
+		this.rank = rank;
+		this.suit = suit;
 	}
-	
-	public String getSuit(){
-		return "Suit = " +suit ;
+
+
+
+	public int getRank() {
+		return rank;
 	}
-	
-	
+
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
