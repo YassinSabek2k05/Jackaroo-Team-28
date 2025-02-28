3ea9013fad1e92b7ae4a78c95d682d7ad05cade5
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-01 00:43:44 +0200

    Fix merge conflicts in Marble class and clean up code

--- src/model/player/Marble.java
+++ src/model/player/Marble.java
@@ -1,11 +1,4 @@
 package model.player;
-<<<<<<< HEAD
-
-=======
-
-
-
->>>>>>> f3cf8064cda82c6992c16393164d3b49fe3546b7
 import model.Colour;
 
 public class Marble {
@@ -13,15 +6,9 @@ public class Marble {
 
 	public Marble(Colour colour) {
 		this.colour = colour;
-
 	}
 	
 	public Colour getColour(){
 		return colour;
-
-<<<<<<< HEAD
-
-=======
 	}
->>>>>>> f3cf8064cda82c6992c16393164d3b49fe3546b7
 }
