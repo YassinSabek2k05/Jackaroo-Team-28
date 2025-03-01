6e01cef68722a126ae39095c2530d1840b58527f
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-01 13:35:31 +0200

    Add CPU player support and initialize player hands in Game class; clean up SafeZone constructor

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -9,6 +9,7 @@ import engine.board.BoardManager;
 import model.Colour;
 import model.card.Card;
 import model.card.Deck;
+import model.player.CPU;
 import model.player.Player;
 
 public class Game implements GameManager{
@@ -28,6 +29,18 @@ public class Game implements GameManager{
         Collections.shuffle(colourOrder);
         this.board = new Board(colourOrder, this);
         Deck.loadCardPool((BoardManager) this.board, this);
-        
+        ArrayList<Player> playersTmp = new ArrayList<>();
+
+        playersTmp.add(new Player(playerName, colourOrder.get(0)));
+        for (int i = 1; i <=3; i++) {
+            playersTmp.add(new CPU("CPU "+i, colourOrder.get(i),(BoardManager) this.board));
+        }
+        this.players = playersTmp;
+        for (Player player : this.players) {
+            player.setHand(Deck.drawCards());
+        }
+        this.currentPlayerIndex = 0;
+        this.firePit = new ArrayList<>();
+
     }
 }
